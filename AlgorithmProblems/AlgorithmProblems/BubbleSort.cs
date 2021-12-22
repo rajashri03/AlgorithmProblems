51ac60e99e9c570f10b21a43f48f8c905debb697
rajashri03 <telvekar.rajashri@gmail.com>
2021-12-22 12:49:16 +0530

    Add-Program to implement bubble sort

---
+++ AlgorithmProblems/AlgorithmProblems/BubbleSort.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProblems
+{
+    /// <summary>
+    /// Program to Implement Bubble Sort
+    /// </summary>
+    public class BubbleSort
+    {
+        /// <summary>
+        /// Method(Bsort) for implementing bubble sort
+        /// </summary>
+        public void BSort()
+        {
+            int[] arr = { 78, 43, 22, 67, 90, 34 };//Declare array
+            int len = arr.Length;//calculate Length of array
+            int i, j, temp;
+            for (i = 0; i < len; i++)
+            {
+                for (j = i + 1; j < len; j++)
+                {
+                    if (arr[j] < arr[i])
+                    {
+                        //swap temp and arr[i];
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                        //prints the array
+                        Console.WriteLine("Sorted array:");
+                        foreach (int sortdata in arr)
+                        {
+                            Console.WriteLine(sortdata);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- AlgorithmProblems/AlgorithmProblems/Program.cs
+++ AlgorithmProblems/AlgorithmProblems/Program.cs
@@ -1,9 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using AlgorithmProblems;
+bool status = true;
+while(status)
+{
+    Console.WriteLine("Algorithms Problems:\n1)Permutation of string\n2)Bubble Sort\n");
+    int op = Convert.ToInt32(Console.ReadLine());
+    switch(op)
+    {
+        case 1:
+            Console.WriteLine("********* Permutations of a String**********");
+            Console.WriteLine("Enter String");
+            var str = Console.ReadLine();
+            int n = str.Length;
+            PermuteString.permute(str, 0, n - 1);
+            break;
+        case 2:
+            Console.WriteLine("********* Bubble Sort**********");
+            BubbleSort b = new BubbleSort();
+            b.BSort();
+            break;
 
-Console.WriteLine("********* Permutations of a String**********");
+    }
+}
 
-Console.WriteLine("Enter String");
-var str = Console.ReadLine();
-int n = str.Length;
-PermuteString.permute(str, 0, n-1);
